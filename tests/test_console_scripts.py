eadfa77ba132ec0fa0bef097c1e65db30464c925
Pierpaolo Pantone <24alsecondo@gmail.com>
2017-02-09 19:06:38 +0100

    Refactor console scripts tests

---
+++ tests/test_console_scripts.py
@@ -0,0 +1,23 @@
+# #/usr/bin/env python
+# #! -*- coding: utf-8 -*-
+
+"""Tests for command-line scripts."""
+
+from language_detection.console_scripts import detect
+
+class TestDetectScript:
+    """Tests for detect.py script."""
+
+    def test_detect_script(self):
+        """Test detect script."""
+
+        args = {
+            'output_file': None,
+            'model': 'output/model.pickle',
+            'predict_args': None,
+            'text': 'This is an english text',
+            'implementation': 'CavnarTrenkleImpl',
+            'loglevel': 30
+        }
+
+        assert detect.start_detection(args) == 'en'
