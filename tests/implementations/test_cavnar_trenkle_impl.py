ac7ec4ded9125acf88b17e0d10baa6f42c9fc16d
Pierpaolo Pantone <24alsecondo@gmail.com>
2017-02-26 21:02:14 +0100

    Refactor CavnarTrenkleImpl tests
    Calls to private functions are not supposed to be tested. Results are now based only on input values and returned results

--- tests/implementations/test_cavnar_trenkle_impl.py
+++ tests/implementations/test_cavnar_trenkle_impl.py
@@ -3,8 +3,6 @@
 
 """Tests for CavnarTrenkleImpl."""
 
-from unittest.mock import Mock
-
 from language_detection.implementations import CavnarTrenkleImpl
 
 
@@ -16,30 +14,33 @@ class TestCavnarTrenkleImpl(object):
         impl = CavnarTrenkleImpl()
 
         # Prepare arguments and mock objects
-        labeled_instances = ['A list', 'of', 'labeled instances']
-        lang_ngram_freqs = {
-            'it': {'c': 1, 'i': 1, 'a': 1, 'o': 1, 'ci': 1, 'ia': 1, 'ao': 1,
-                   'cia': 1, 'iao': 1, 'ciao': 1},
-            'en': {'h': 1, 'e': 1, 'l': 2, 'o': 1, 'he': 1, 'el': 1, 'll': 1,
-                   'lo': 1, 'hel': 1, 'ell': 1, 'llo': 1, 'hell': 1, 'ello': 1,
-                   'hello': 1}
+        labeled_instances = [
+            {
+                'id_str': '123456168300273664',
+                'language': 'it',
+                'text': 'Ciao'
+            },
+            {
+                'id_str': '123456168300273665',
+                'language': 'en',
+                'text': 'hello'
+            }]
+
+        # Call the tested method without specifying limit value
+        result = impl.train(labeled_instances, limit=None)
+        expected = {
+            'en': ['l', 'o', 'lo', 'llo', 'll', 'hello', 'hell', 'hel', 'he',
+                   'h', 'ello', 'ell', 'el', 'e'],
+            'it': ['o', 'iao', 'ia', 'i', 'ciao', 'cia', 'ci', 'c', 'ao', 'a']
         }
 
-        impl._languages_ngram_frequencies = Mock(
-            return_value=lang_ngram_freqs)
+        assert result == expected
 
-        impl._compute_profile_from_frequencies = Mock(
-            return_value='Profiles')
+        # Call the tested method using a limit
+        result = impl.train(labeled_instances, limit=8)
+        expected = {
+            'en': ['l', 'o', 'lo', 'llo', 'll', 'hello', 'hell', 'hel'],
+            'it': ['o', 'iao', 'ia', 'i', 'ciao', 'cia', 'ci', 'c']
+        }
 
-        # Call the tested method
-        result = impl.train(labeled_instances, limit=10)
-
-        # Assertions
-        impl._languages_ngram_frequencies.assert_called_once_with(
-            ['A list', 'of', 'labeled instances'])
-
-        assert impl._compute_profile_from_frequencies.call_count == 2
-
-        # Make sure the result is the full dict obtained by looping over
-        # the languages and calling _compute_profile_from_frequencies
-        assert result == {'en': 'Profiles', 'it': 'Profiles'}
+        assert result == expected
